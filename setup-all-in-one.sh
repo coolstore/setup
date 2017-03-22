521e3dd97576f6fa0da52a7ce465c60d68f7d255
tqvarnst <tqvarnst@redhat.com>
2017-03-22 17:39:48 +0100

    fixed issues with the all in one script

--- setup-all-in-one.sh
+++ setup-all-in-one.sh
@@ -11,6 +11,7 @@ MODULE_NAME=    #This is not a module, but a use-case script
 
 if [ ! -f ${BASE_DIR}/common/common.sh ]; then
   echo "Missing file ../common/common.sh. Please make sure that all required modules are downloaded or run the download.sh script."
+  exit
 fi
 
 source ${BASE_DIR}/common/common.sh
@@ -26,30 +27,30 @@ fi
 
 # Product Catalog
 echo_header "Bulding and deploying the Product Catalog service"
-$BASE_DIR/coolstore-catalog/deploy.sh ${PARAMS} > /dev/null
+$BASE_DIR/catalog/deploy.sh ${PARAMS} > /dev/null
 
 # Product Inventory
 echo_header "Buiding and deploying the Inventory service"
-$BASE_DIR/coolstore-inventory/deploy.sh ${PARAMS} > /dev/null
+$BASE_DIR/inventory/deploy.sh ${PARAMS} > /dev/null
 
 # Shopping-cart
 echo_header "Buiding and deploying the Cart service"
-$BASE_DIR/coolstore-shoppingcart/deploy.sh ${PARAMS} > /dev/null
+$BASE_DIR/shoppingcart/deploy.sh ${PARAMS} > /dev/null
 
 # Gateway
 echo_header "Buiding and deploying the Gateway service"
-$BASE_DIR/coolstore-gateway/deploy.sh ${PARAMS} > /dev/null
+$BASE_DIR/gateway/deploy.sh ${PARAMS} > /dev/null
 
 if $INSTALL_SSO; then
   echo_header "Deploy Red Hat Single Sign-on"
-  $BASE_DIR/coolstore-sso/deploy.sh ${PARAMS} > /dev/null
+  $BASE_DIR/sso/deploy.sh ${PARAMS} > /dev/null
   # UI
   echo_header "Buiding and deploying the WEB UI service"
-  $BASE_DIR/coolstore-ui/deploy.sh ${PARAMS} -g ${COOLSTORE_GW_ENDPOINT} -s ${SSO_URL}> /dev/null
+  $BASE_DIR/web-ui/deploy.sh ${PARAMS} -g ${COOLSTORE_GW_ENDPOINT} -s ${SSO_URL}> /dev/null
 else 
   # UI
   echo_header "Buiding and deploying the WEB UI service"
-  $BASE_DIR/coolstore-ui/deploy.sh ${PARAMS} -g ${COOLSTORE_GW_ENDPOINT} > /dev/null
+  $BASE_DIR/web-ui/deploy.sh ${PARAMS} -g ${COOLSTORE_GW_ENDPOINT} > /dev/null
 fi
 
 
